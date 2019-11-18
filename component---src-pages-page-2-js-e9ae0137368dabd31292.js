05a727f7d8fc420366de635ab4c688c92ca8ed05
Mubashir Cheentavida <msbmubashir@gmail.com>
2019-11-17 23:41:37 -0500

    Update

---
+++ component---src-pages-page-2-js-e9ae0137368dabd31292.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{225:function(e,t,a){"use strict";a.r(t);var n=a(0),o=a.n(n),c=a(28),l=a(220),r=a(221);t.default=function(){return o.a.createElement(l.a,null,o.a.createElement(r.a,{title:"Resources"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(c.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-e9ae0137368dabd31292.js.map
